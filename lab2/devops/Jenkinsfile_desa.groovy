74cf141bc47df06d002a850c4ef0d6225fad51f6
andresrdu <andresrdu@hotmail.com>
2019-11-08 17:20:50 -0500

    first commit

--- lab2/devops/Jenkinsfile_desa.groovy
+++ lab2/devops/Jenkinsfile_desa.groovy
@@ -15,9 +15,18 @@ try {
           }
       }
       stage('Build Container') {      
-         
-          
-     
+         //dockerBuild = ""
+         withCredentials([string(credentialsId:"andresrdu-sp",variable:'azcre')]){
+            //token : CLIENT_ID|CLIENT_SECRET|TENANT_ID|SUSCRIPTION_ID
+            spcredential = azcre.split('\\|')
+            clientId = spcredential[0]
+            clientSe = spcredential[1]
+            tennatId = spcredential[2]
+            subscrId = spcredential[3]
+            sh "docker build -t az-demo:1.0 --build-arg CLIENT_ID=${clientId} --build-arg CLIENT_SECRET=${clientSe} --build-arg TENANT_ID=${tennatId} --build-arg SUSCRIPTION_ID=${subscrId}"
+
+            //sh "${dockerBuild}"
+         }
    }
    
 } 
